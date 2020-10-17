b633dc0a0ef500f7baadaf8cb89237374236de07
vsidera <siderravictor@gmail.com>
2020-10-17 11:25:52 +0300

    add login functionality

--- Vinstagram/settings.py
+++ Vinstagram/settings.py
@@ -130,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'vinsta-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -17,3 +15,7 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')    
