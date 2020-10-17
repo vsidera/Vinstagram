1614c83d1ea7a4b4c39e901de49d0efdbac1c4c0
vsidera <siderravictor@gmail.com>
2020-10-17 07:05:28 +0300

    add Image model

--- vinsta/models.py
+++ vinsta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    ''' a model for Image posts '''
+    image = models.ImageField(upload_to='images/')
+    caption = models.TextField()
+    likes = models.ManyToManyField(User, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
