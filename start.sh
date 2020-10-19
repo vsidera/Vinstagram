fcee777acfcc7f257addce6dbca718c49638c67b
vsidera <siderravictor@gmail.com>
2020-10-19 11:28:01 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-DB_NAME=de2paje6c6cq4u
-DB_USER=amsecmhvrwtpck
-DB_PASSWORD=d7a0beb2ec0dc7cead13cc39a1fdc99e0b8bf3ad73bcb1cdf851aeae01c7e44a
-DB_HOST=ec2-100-25-100-81.compute-1.amazonaws.com
-SECRET_KEY=7i2-%dzi*nsj3l0mrvz)k*o7faosq042_r+k6t5hv1@&(k67w4
+DB_NAME='de2paje6c6cq4u'
+DB_USER='amsecmhvrwtpck'
+DB_PASSWORD='d7a0beb2ec0dc7cead13cc39a1fdc99e0b8bf3ad73bcb1cdf851aeae01c7e44a'
+DB_HOST='ec2-100-25-100-81.compute-1.amazonaws.com'
+SECRET_KEY='7i2-%dzi*nsj3l0mrvz)k*o7faosq042_r+k6t5hv1@&(k67w4'
 DB_PORT=5432
 DISABLE_COLLECTSTATIC=1
 DEBUG=False
