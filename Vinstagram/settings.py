de74928697a107ac7d640f733235497bb615d126
vsidera <siderravictor@gmail.com>
2020-10-19 11:53:50 +0300

    deployment

--- Vinstagram/settings.py
+++ Vinstagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7i2-%dzi*nsj3l0mrvz)k*o7faosq042_r+k6t5hv1@&(k67w4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
